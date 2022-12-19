fd10e957e42a83d674afe5848ca41d798cd09c8e
MasterMod22 <99485542+MasterMod22@users.noreply.github.com>
2022-12-19 06:01:39 -0500

    uhhh

--- Vanilla Client/Modules/OnplayerMenu.cs
+++ Vanilla Client/Modules/OnplayerMenu.cs
@@ -19,7 +19,7 @@ namespace Vanilla.Modules
             new QMNestedButton(tabMenu, 1, 4, "selectedUserMenu", "Vanilla", "Vanilla Client");
             new QMSingleButton(selectedUserMenu, 1, 0, "Target Player", delegate
             {
-                PlayerWrapper.Target.Targetuser(GameObject.Find("/_Application").transform.Find("UIManager/SelectedUserManager").gameObject.GetComponent<VRC.DataModel.UserSelectionManager>().field_Private_APIUser_1.id));
+                PlayerWrapper.Target.Targetuser(GameObject.Find("/_Application").transform.Find("UIManager/SelectedUserManager").gameObject.GetComponent<VRC.DataModel.UserSelectionManager>().field_Private_APIUser_1.id);
             }, "Target User");
         }
 
