0dcf7da633da9a5197c619067f1bb2f26fdfadc9
Cypher <i@hvl.gg>
2022-12-18 23:20:29 -0500

    Fix

--- Vanilla Client/Modules/ESPModule.cs
+++ Vanilla Client/Modules/ESPModule.cs
@@ -8,15 +8,12 @@ using Vanilla.Config;
 using VRC;
 using VRC.Core;
 
-namespace Vanilla.Modules
+namespace Vanilla.Modules  
 {
     internal class ESPModule : VanillaModule
     {
-        protected override string ModuleName => "ESPMdule";
-
         internal override void PlayerJoin(Player __0)
         {
-
             if (!MainConfig.ESP || __0.field_Private_APIUser_0.id == APIUser.CurrentUser.id)
                 return;
 
@@ -24,6 +21,6 @@ namespace Vanilla.Modules
             var Renderer = __0.transform.Find("SelectRegion").GetComponent<Renderer>();
             HighlightsFX.field_Private_Static_HighlightsFX_0.Method_Public_Void_Renderer_Boolean_0(Renderer, true);
         }
-
+       
     }
 }
